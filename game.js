94d9e76ef26ff299a577376fa1353eec824e482f
JoshGuarino <guar0638@fredonia.edu>
2018-04-18 20:08:39 -0400

    4/18/18 final changes for day

--- game.js
+++ game.js
@@ -10,15 +10,18 @@
 // Dealer Hits on 16 and Below
 // 
 
-var player1 = {name: 'huey', money: 200, active:true, handCount: 0, hand: []}
-var player2 = {name: 'Dewey', money: 200, active: true, handCount: 0, hand: []}
-var player3 = {name: 'Lewey', money: 200, active: true, handCount: 0, hand: []}
-var player4 = {name: 'Scrooge', money: 200, active: true, handCount: 0, hand: []}
-var dealer = {name: 'The House', money: 0, active: true, handCount: 0, hand: []}
+// var player1 = {name: 'huey', money: 200, active:true, handCount: 0, hand: []}
+// var player2 = {name: 'Dewey', money: 200, active: true, handCount: 0, hand: []}
+// var player3 = {name: 'Lewey', money: 200, active: true, handCount: 0, hand: []}
+// var player4 = {name: 'Scrooge', money: 200, active: true, handCount: 0, hand: []}
 
-//var players = [player1, player2, player3, player4, dealer];
+var players = [player1 = {name: 'huey', money: 200, active:true, handCount: 0, hand: []},
+               player2 = {name: 'Dewey', money: 200, active: true, handCount: 0, hand: []},
+               player3 = {name: 'Lewey', money: 200, active: true, handCount: 0, hand: []},
+               player4 = {name: 'Scrooge', money: 200, active: true, handCount: 0, hand: []}];
 
-const _numberOfDecks = 1; // 1-8 max  Math.floor(Math.random()*8)
+var dealer = {name: 'The House', money: 0, active: true, handCount: 0, hand: []};
+const _numberOfDecks = Math.floor(Math.random()*8)+1; // 1-8 max  
 const _rounds = 20;
 var suits = ['hearts', 'diamonds','spades','clubs'];
 var card = ["ace",'2','3','4','5','6','7','8','9','10',"jack","queen","king"];
@@ -26,7 +29,7 @@ var deck = [];
 
 
 //initialize and shuffle deck
-function shuffleDeck(Array)
+function shuffleDeck(decks)
 {
     for(let j=1; j<=_numberOfDecks; j++)
     {
@@ -36,85 +39,125 @@ function shuffleDeck(Array)
             {
                 if(card[n]==="2" || card[n]==="3" || card[n]==="4" || card[n]==="5" || card[n]==="6" || card[n]==="7" || card[n]==="8" || card[n]==="9")
                 {
-                    Array.push({suit: suits[i], cardValue: card[n], numValue: n+1});
+                    decks.push({suit: suits[i], cardValue: card[n], numValue: n+1});
                 }
                 else if(card[n]==="jack" || card[n]==="queen" || card[n]==="king" || card[n]==="10")
                 {
-                    Array.push({suit: suits[i], cardValue: card[n], numValue: 10});
+                    decks.push({suit: suits[i], cardValue: card[n], numValue: 10});
                 }
                 else
                 {
-                    Array.push({suit: suits[i], cardValue: card[n], numValue: 11});
+                    decks.push({suit: suits[i], cardValue: card[n], numValue: 11});
                 }
             }
         }
     }
-    function shuffleCard(Array)
+    let arrLength = decks.length;
+    for(let i=0; i<arrLength; i++)
+    {
+        if(decks[i].suit === 'hearts' || decks.suit === 'diamonds')
+        {
+            decks[i].color = "red";
+        }
+        else
+        {
+            decks[i].color = "black";
+        }
+    }
+    function shuffleCard(decks)
     {
         let i = 0, j = 0, temp = null;
 
-        for (i = Array.length - 1; i > 0; i--)
+        for (i = decks.length - 1; i > 0; i--)
         {
-            j = Math.floor(Math.random() * (Array.length));
-            temp = Array[i];
-            Array[i] = Array[j];
-            Array[j] = temp;
+            j = Math.floor(Math.random() * (decks.length));
+            temp = decks[i];
+            decks[i] = decks[j];
+            decks[j] = temp;
         }
     }
-    shuffleCard(Array);
-    return Array;   
+    shuffleCard(decks);
+    return decks;   
 } 
 
 
 //inital deal
-function initalDeal(Object)
+function initalDeal(player)
 {
-    if(Object.active===true)
+    if(player.active===true)
     {
-        Object.hand.push(deck.pop());  Object.hand.push(deck.pop());     
-        Object.handCount = Object.hand[0].numValue + Object.hand[1].numValue;      
+        player.hand.push(deck.pop());  player.hand.push(deck.pop());     
+        player.handCount = player.hand[0].numValue + player.hand[1].numValue;      
     }
-    return Object;
+    return player;
 }    
 
 //players place there bets
-function placeBets(Object)
+function placeBets(player)
 {
-    if(Object.active===true)
+    if(player.active===true)
     {
-        Object.money = Object.money-15;
+        player.money = player.money-15;
         dealer.money = dealer.money+15;
     }
-    return Object;
+    return player;
 }
    
 
 //player turn
-function playerTurn(Object)
+function playerTurn(player)
 {
-    if(Object.active===true)
+    if(player.active===true)
     {
         let countNum = 2;
-        while(Object.handCount <= 16)
+        while(player.handCount <= 16)
         {
-            Object.hand.push(deck.pop());
-            Object.handCount = Object.handCount + Object.hand[countNum].numValue;
-            if(Object.handCount > 21)
+            player.hand.push(deck.pop());
+            player.handCount = player.handCount + player.hand[countNum].numValue;
+            if(player.handCount > 21)
             {
-                let handCount = Object.hand.length;
+                let handCount = player.hand.length;
                 for(let i=0; i < handCount; i++)
                 {
-                    if(Object.hand[i].cardValue==="ace")
+                    if(player.hand[i].cardValue==="ace")
                     {
-                        Object.hand[i].numValue = 1;
+                        player.hand[i].numValue = 1;
                     }
                 }
             }
             countNum++;
         }
     }
-    return Object;
+    return player;
 }
+
+//dealer turn
+function dealerTurn(dealer)
+{
+    if(dealer.active===true)
+    {
+        let countNum = 2;
+        while(dealer.handCount <= 16)
+        {
+            dealer.hand.push(deck.pop());
+            dealer.handCount = dealer.handCount + dealer.hand[countNum].numValue;
+            if(dealer.handCount > 21)
+            {
+                let handCount = dealer.hand.length;
+                for(let i=0; i < handCount; i++)
+                {
+                    if(dealer.hand[i].cardValue==="ace")
+                    {
+                        dealer.hand[i].numValue = 1;
+                    }
+                }
+            }
+            countNum++;
+        }
+    }
+    return dealer;
+}
+
 
 // determine outcomes
 function determineOutcome(Object)
@@ -139,76 +182,82 @@ function determineOutcome(Object)
 
 
 //clear player hands
-function clearHand(Object)
+function clearHand(player)
 {
-    Object.handCount = 0;
-    let handLength = Object.hand.length;
+    player.handCount = 0;
+    let handLength = player.hand.length;
     for(let i=0; i < handLength; i++)
     {
-        Object.hand.pop();
+        player.hand.pop();
     }
-    return Object;
+    return player;
 }
 
 //clear deck
-function clearDeck(Array)
+function clearDeck(decks)
 {
-    var deckLength = Array.length;
+    var deckLength = decks.length;
     for(let i=0; i < deckLength; i++)
     {
-        Array.pop();
+        decks.pop();
     }
-    return Array;
+    return decks;
 }
 
 
 
-//shuffleDeck(deck);
-//console.log(deck);
+// shuffleDeck(deck);
+// console.log(deck);
 
 
 //main game structure
 for(let n=0; n < _rounds; n++)
 {
      shuffleDeck(deck);
-
+    var playersLength = players.length;
     //place bets
-    placeBets(player1);
-    placeBets(player2);
-    placeBets(player3);
-    placeBets(player4);
+    for(let i=0; i<playersLength; i++)
+    {
+        placeBets(players[i]);
+    }
+    //players.forEach( Object => { placeBets(Object) });
+ 
 
-    //deal the cards
-    initalDeal(player1);
-    initalDeal(player2);
-    initalDeal(player3);
-    initalDeal(player4);
+    // //deal the cards
+    for(let i=0; i<playersLength; i++)
+    {
+        initalDeal(players[i]);
+    }
     initalDeal(dealer);
 
     //each player takes their turn
-    playerTurn(player1);
-    playerTurn(player2);
-    playerTurn(player3);
-    playerTurn(player4);
-    playerTurn(dealer);
+    for(let i=0; i<playersLength; i++)
+    {
+        playerTurn(players[i]);
+    }
+    dealerTurn(dealer);
    
     //dtermine the outcome of the hand
-    determineOutcome(player1);
-    determineOutcome(player2);
-    determineOutcome(player3);
-    determineOutcome(player4);
-
+    for(let i=0; i<playersLength; i++)
+    {
+        determineOutcome(players[i]);
+    }
 
     //take back the cards
-    clearHand(player1);
-    clearHand(player2);
-    clearHand(player3);
-    clearHand(player4);
+    for(let i=0; i<playersLength; i++)
+    {
+        clearHand(players[i]);
+    }
     clearHand(dealer);
 
-    
     //clear deck
     clearDeck(deck);
+
+    if(players[0].active===false && players[1].active===false && players[2].active===false && players[3].active===false)
+    {
+        break;
+    }
+
 }
 
 
